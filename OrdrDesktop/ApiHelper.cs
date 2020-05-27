98e9710dc0be8c8227a7f54a9889326e558e3ca5
guiguat <guaturadzn@gmail.com>
2020-05-27 01:59:37 -0300

    :sparkles: Create api class

---
+++ OrdrDesktop/ApiHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdrDesktop
+{
+    class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.BaseAddress = new Uri("http://localhost:3333/");  
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
