babd9c6c621f52571b878a286a7b57a690abf340
guiguat <guaturadzn@gmail.com>
2020-05-27 02:25:49 -0300

    :sparkles: Create model Relatorio

---
+++ OrdrDesktop/models/RelatorioModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdrDesktop.models
+{
+    public class RelatorioModel
+    {
+        public List<RelatorioItemModel> relatorios { get; set; }
+    }
+
+    public class RelatorioItemModel
+    {
+        public string DataHora { get; set; }
+        public float Debito { get; set; }
+        public float Credito { get; set; }
+        public float Dinheiro { get; set; }
+        public float Total { get; set; }
+
+    }
+}
