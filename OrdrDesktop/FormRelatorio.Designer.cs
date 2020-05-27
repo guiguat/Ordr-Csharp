c06bba4c7ea7010d96acb501f6151000f2099cab
guiguat <guaturadzn@gmail.com>
2020-05-27 01:03:59 -0300

    :fire: Delete form2 and create FormRelatorio

--- OrdrDesktop/FormRelatorio.Designer.cs
+++ OrdrDesktop/FormRelatorio.Designer.cs
@@ -28,35 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(250, 182);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Relatorios";
             // 
             // FormRelatorio
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 393);
-            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(577, 432);
             this.Name = "FormRelatorio";
+            this.Text = "FormRelatorio";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
     }
 }
