9653296381af2882c23a0be0ff674c6cb367fa15
guiguat <guaturadzn@gmail.com>
2020-06-02 17:44:58 -0300

    :sparkles: create design produtos

--- OrdrDesktop/FormProduto.Designer.cs
+++ OrdrDesktop/FormProduto.Designer.cs
@@ -29,34 +29,206 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelForm = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numPreco = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbPrato = new System.Windows.Forms.CheckBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panelForm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPreco)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Poppins", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.Size = new System.Drawing.Size(229, 34);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Produto";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Text = "Cadastro de Produtos";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(561, 48);
+            this.panel1.TabIndex = 1;
+            // 
+            // panelForm
+            // 
+            this.panelForm.Controls.Add(this.numericUpDown2);
+            this.panelForm.Controls.Add(this.label6);
+            this.panelForm.Controls.Add(this.cbPrato);
+            this.panelForm.Controls.Add(this.label5);
+            this.panelForm.Controls.Add(this.numericUpDown1);
+            this.panelForm.Controls.Add(this.label4);
+            this.panelForm.Controls.Add(this.numPreco);
+            this.panelForm.Controls.Add(this.label3);
+            this.panelForm.Controls.Add(this.txbName);
+            this.panelForm.Controls.Add(this.label2);
+            this.panelForm.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelForm.Location = new System.Drawing.Point(0, 48);
+            this.panelForm.Name = "panelForm";
+            this.panelForm.Size = new System.Drawing.Size(561, 88);
+            this.panelForm.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(257, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nome:";
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(300, 16);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(165, 20);
+            this.txbName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Preço:";
+            // 
+            // numPreco
+            // 
+            this.numPreco.Location = new System.Drawing.Point(55, 47);
+            this.numPreco.Name = "numPreco";
+            this.numPreco.Size = new System.Drawing.Size(120, 20);
+            this.numPreco.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(277, 49);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Estoque:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(443, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tipo:";
+            // 
+            // cbPrato
+            // 
+            this.cbPrato.AutoSize = true;
+            this.cbPrato.Location = new System.Drawing.Point(481, 51);
+            this.cbPrato.Name = "cbPrato";
+            this.cbPrato.Size = new System.Drawing.Size(51, 17);
+            this.cbPrato.TabIndex = 7;
+            this.cbPrato.Text = "Prato";
+            this.cbPrato.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(108, 16);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Código (del/edit):";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 136);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(561, 257);
+            this.panel2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(561, 257);
+            this.dataGridView1.TabIndex = 0;
             // 
             // FormProduto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(561, 393);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelForm);
+            this.Controls.Add(this.panel1);
             this.Name = "FormProduto";
             this.Text = "FormProduto";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelForm.ResumeLayout(false);
+            this.panelForm.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numPreco)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelForm;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numPreco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbPrato;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- OrdrDesktop/FormProduto.cs
+++ OrdrDesktop/FormProduto.cs
@@ -16,5 +16,6 @@ namespace OrdrDesktop
         {
             InitializeComponent();
         }
+
     }
 }
