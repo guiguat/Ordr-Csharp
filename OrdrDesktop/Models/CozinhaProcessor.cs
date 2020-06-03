db7531162172a00927136e34d01fa304ba7e7eb8
guiguat <guaturadzn@gmail.com>
2020-06-03 11:15:22 -0300

    :sparkles: cozinha data models

---
+++ OrdrDesktop/Models/CozinhaProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OrdrDesktop.Models
+{
+    class CozinhaProcessor
+    {
+        public static async Task<CozinhaModel> getProdutos()
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync($"{ApiHelper.baseUrl}/pedido"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    CozinhaModel relatorio = await response.Content.ReadAsAsync<CozinhaModel>();
+                    return relatorio;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+
+    }
+    class CozinhaModel : List<CozinhaItemModel>
+    {
+
+    }
+
+    class CozinhaItemModel
+    {
+        public int Id {get;set;}
+        public int Mesa { get; set; }
+        public string Pedidos { get; set; }
+        public DateTime DataHora { get; set; }  
+
+    }
+}
