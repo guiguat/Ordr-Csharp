50ed8bb5a67158bd6074f8e826cbc9eb4db875e2
guiguat <guaturadzn@gmail.com>
2020-06-03 07:53:18 -0300

    remove unnecessary using

--- OrdrDesktop/Models/RelatorioProcessor.cs
+++ OrdrDesktop/Models/RelatorioProcessor.cs
@@ -1,9 +1,6 @@
 ﻿using OrdrDesktop.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OrdrDesktop
